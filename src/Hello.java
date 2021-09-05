8562501033774619fbf690f414bc0d05d3b02a2a
zhuoranlzr <zhuoran3@asu.edu>
2021-09-05 09:26:51 -0700

    芜湖起飞

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello taka8rie");
         System.out.println("Roger that");
+        System.out.println("芜湖起飞");
     }
 }
